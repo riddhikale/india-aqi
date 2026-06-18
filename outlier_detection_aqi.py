5ae32d4437e204e8fd2267daff0a90c5029fc67e
riddhikale <riddhiskale28@gmail.com>
2026-06-18 11:46:43 +0530

    implement q1 and q3 calculations in outlier detection function

--- outlier_detection_aqi.py
+++ outlier_detection_aqi.py
@@ -10,3 +10,28 @@ print(f"Delhi rows: {len(delhi)}")
 def detect_outliers(data, label):
     sorted_data = np.sort(data)
     n = len(sorted_data)
+
+    lower_half = sorted_data[:n//2]
+    upper_half = sorted_data[n//2 + (n%2):]
+
+
+    # Q1 = median of lower_half
+    m = len(lower_half)
+    if m % 2 == 1:
+        q1 = lower_half[n // 2]
+    else:
+        mid = m // 2
+        q1 = (lower_half[mid-1] + lower_half[mid]) / 2
+    
+    # Q3 = median of upper_half
+    m = len(upper_half)
+    if m % 2 == 1:
+        q3 = upper_half[n // 2]
+    else:
+        mid = m // 2
+        q3 = (upper_half[mid-1] + upper_half[mid]) / 2
+    
+   
+
+
+    
