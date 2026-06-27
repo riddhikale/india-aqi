07977dd18a139e5e3e2f1b85826fa6237d6131f1
riddhikale <riddhiskale28@gmail.com>
2026-06-27 15:24:56 +0530

    add correlation matrix calculation for pollutants

--- pollutant_correlation_heatmap.py
+++ pollutant_correlation_heatmap.py
@@ -5,3 +5,7 @@ import seaborn as sns
 
 df = pd.read_csv("data.csv", encoding="cp1252", low_memory=False)
 print(df.head())
+
+pollutant_cols = ["so2", "no2", "rspm", "spm", "pm2_5"]
+corr_matrix = df[pollutant_cols].corr()
+
